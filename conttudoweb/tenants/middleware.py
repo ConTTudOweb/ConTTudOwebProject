925db2370899762176e907a4905f87722961f9ad
Alessandro de Lima Folk <sandrofolk@hotmail.com>
2020-05-21 19:48:31 -0300

    Correções

--- conttudoweb/tenants/middleware.py
+++ conttudoweb/tenants/middleware.py
@@ -10,8 +10,6 @@ class TenantMiddleware(BaseTenantMiddleware):
     def get_tenant(self, model, hostname, request):
         public_schema_name = get_public_schema_name()
         schema_name = request.headers.get('Tenant', public_schema_name)
-        print('Tenant: ', schema_name)
-        print(hostname, request.headers)
         if schema_name != public_schema_name:
             return model.objects.get(schema_name=schema_name)
 
